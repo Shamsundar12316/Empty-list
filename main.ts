07dbad359a01612989e7a904d6d8a4eb78c630eb
Shamsundar12316 <168196650+Shamsundar12316@users.noreply.github.com>
2024-08-06 03:36:28 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,13 @@
+// part 26 // question 31
+// no user; add an if test to exercise 30 to make sure the list of users is not empty
+// if the list is empty , print the message we need to find some users!
+// remove all of the username from your array,and make sure the correct message is printed
+
+let users: string[] = []
+
+if(users.length > 0){
+    console.log(`we have enough users`)
+}
+else{
+    console.log(`we need find some users!`)
+}
